7f559b92016f583617aa7c94f7c63ae38a6510fb
ishii <coffeeya3@gmail.com>
2016-08-28 18:39:35 +0900

    first code commit

---
+++ SampleProject/Assets/firstScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class firstScript : MonoBehaviour {
+    public Text text;
+
+	// Use this for initialization
+	void Start () {
+        text.text = "こんにちは";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
