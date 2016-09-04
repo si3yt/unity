3b2a78b5cb38da56405a912b5c3513c280f6a10d
ishii <coffeeya3@gmail.com>
2016-09-04 16:36:57 +0900

    first sample commit

---
+++ SampleProject2/Assets/Scripts/Ball.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        // 左上向きの速度の設定
+        this.GetComponent<Rigidbody>().velocity = new Vector3(-10.0f, 9.0f, 0.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    // Ball destroy when this out of screen
+    void OnBecameInvisible() //ゲームオブジェクトが画面の外に移動して描画されなくなった時に呼ばれる
+    {
+        Destroy(this.gameObject);
+    }
+}
---
+++ SampleProject2/Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+    protected float jump_speed = 12.0f;
+    public bool is_landing = false;
+
+	// Use this for initialization
+	void Start () {
+        this.is_landing = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (this.is_landing)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                this.is_landing = false;
+                this.GetComponent<Rigidbody>().velocity =
+                    Vector3.up * this.jump_speed;
+            }
+        }
+	}
+
+    // this game object collision others
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Floor")
+        {
+            this.is_landing = true;
+        }
+    }
+}
